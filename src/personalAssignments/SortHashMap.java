f3993354581eea2977b6d6c94a766eb496a3a97e
Revask <srebu.2017@gmail.com>
2023-03-30 12:55:08 -0700

    added testgit java and changed HashmaoSorting

--- src/personalAssignments/SortHashMap.java
+++ src/personalAssignments/SortHashMap.java
@@ -3,19 +3,21 @@ package personalAssignments;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
 public class SortHashMap {
 
 	static HashMap<String,Integer> map = new HashMap<String,Integer>();
 	public static void main(String args[]) {
-	map.put("Rebu",10);
+	 System.out.println("This is HashMap Sorting");
+		map.put("Rebu",10);
 	map.put("Arjun",12);
 	map.put("Suresh", null);
 	map.put(null, null);
 	map.put(null, 12);
 	map.put(null,13);
 	map.put("Siddhu",15);
-	Set<String> s = new HashSet<String>(map.keySet());
+	Set<Entry<String,Integer>> 	s = new HashSet<Entry<String,Integer>>(map.entrySet());
 	Iterator itr = s.iterator();
 	while(itr.hasNext())
 		System.out.println("Key : " + itr.next());
---
+++ src/personalAssignments/TestGit.java
@@ -0,0 +1,7 @@
+package personalAssignments;
+
+public class TestGit {
+public static void main(String args[])
+   {System.out.println("test");
+	}
+}
